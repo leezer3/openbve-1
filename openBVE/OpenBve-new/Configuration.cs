dbf9be94df2c0e02f0a7fa58d358be5c6b7c1e84
Jakub Vaněk <vanek.jakub4@seznam.cz>
2015-07-07 19:39:57 +0200

    Misc modifications (GameFiles, compile-time config)

--- openBVE/OpenBve-new/Configuration.cs
+++ openBVE/OpenBve-new/Configuration.cs
@@ -2,10 +2,18 @@
 
 namespace OpenBve
 {
+	/// <summary>
+	/// Compile-time configuration.
+	/// </summary>
     internal static class Configuration
     {
-		// TODO
+		/// <summary>
+		/// List of paths for looking up the <see cref="FileSysCfg"/> file. If empty, the default location is used.
+		/// </summary>
 		internal static readonly string[] FileSysCfgPaths = {};
+		/// <summary>
+		/// Default Filesystem configuration file name.
+		/// </summary>
 		internal static readonly string FileSysCfg = "filesystem.cfg";
     }
 }
