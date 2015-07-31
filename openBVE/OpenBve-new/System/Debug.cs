045d7ab46cf976e8f08c482435a799a4e0d2b497
Jakub Vaněk <vanek.jakub4@seznam.cz>
2015-07-31 16:42:31 +0200

    Convert Debug class' internal storage to List

---
+++ openBVE/OpenBve-new/System/Debug.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenBve
+{
+	internal static class Debug
+    {
+		// messages
+		internal enum MessageType {
+			Warning,
+			Error,
+			Critical
+		}
+		internal struct Message {
+			internal MessageType Type;
+			internal bool FileNotFound;
+			internal string Text;
+			public Message(MessageType Type, bool FileNotFound, string Text){
+				this.Type = Type;
+				this.FileNotFound = FileNotFound;
+				this.Text = Text;
+			}
+		}
+		internal static List<Message> Messages = null;
+		internal static void AddMessage(MessageType Type, bool FileNotFound, string Text) {
+			if (Type == MessageType.Warning & !Options.Current.ShowWarningMessages) return;
+			if (Type == MessageType.Error & !Options.Current.ShowErrorMessages) return;
+			if (Messages == null) {
+				Messages = new List<Message>(16);
+			}
+			Message m = new Message(Type, FileNotFound, Text);
+			Messages.Add(m);
+
+			Program.AppendToLogFile(Text);
+
+		}
+		internal static void ClearMessages() {
+			Messages.Clear();
+		}
+    }
+}
+
